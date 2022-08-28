165891d1c9dbcb2423f3733b8691e79c66da7015
mawa <mawahama05@gmail.com>
2022-08-27 23:08:34 -0700

    completed assignment

---
+++ index.js
@@ -0,0 +1,41 @@
+function add(a, b){
+ return a + b
+}
+function subtract(a, b){
+  return a - b
+}
+function multiply(a, b){
+ return a * b
+}
+function divide(a, b){
+ return a / b
+}
+function increment(n){
+    const result = n+1 ;
+    return result;
+}
+function decrement(n){
+    const result = n-1;
+    return result
+}
+function makeInt(n){
+    n.toString(parseInt(n));
+    return n;
+}
+function makeInt(n){
+    n.toString(parseInt(n = 0));
+    return n;
+}
+
+function makeInt(n){
+    n.toString(parseInt(n = 10));
+    return n;
+}
+function makeInt(n){
+    n.toString(parseInt(true));
+    return n;
+}
+function preserveDecimal(n){
+    const result = parseFloat(n);
+    return result;
+}
